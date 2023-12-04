ddb48de5cc50ee284381dc98cc2fa9e4a1cae356
tscar-1 <71184141+tscar-1@users.noreply.github.com>
2023-12-04 13:19:11 -0500

    Update project3.py

--- project3.py
+++ project3.py
@@ -1,5 +1,34 @@
-def gaussSeidel():
-  
+def gaussSeidel(augMtx, delta, flag):
+  n = len(augMtx)
+  prevSoln = [0] * n
+  currSoln = [0] * n
+
+  while True:
+    mae = 0
+    rmse = 0
+    
+    for i in range(n):
+    sum = 0
+    for j in range(n):
+      if i != j:
+        sum += augMtx[i][j] * currSoln[j]
+    currSoln[i] = (augMtx[i][n] - sum / augMtx[i][i]
+
+    for i in range(n):
+      mae += abs(currSoln[i] - prevSoln[i])
+      rmse += math.pow(currSoln[i] - prevSoln[i], 2)
+
+    if flag == 1:
+      pass
+    elif flag == 2:
+      pass
+    elif flag == 3:
+      pass
+
+    if mae < delta or rmse < delta:
+      break
+
+  return currSoln
 
 def jacobi():
   
